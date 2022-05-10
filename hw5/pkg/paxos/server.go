f2abc0d7e2b89b501a801bef54fb2eb74998f93c
Tushar Gupta <tg2749@columbia.edu>
2022-05-10 07:26:45 -0400

    Completed Part B

--- hw5/pkg/paxos/server.go
+++ hw5/pkg/paxos/server.go
@@ -69,11 +69,90 @@ func NewServer(peers []base.Address, me int, proposedValue interface{}) *Server 
 func (server *Server) MessageHandler(message base.Message) []base.Node {
 	//TODO: implement it
 
-	newNodes := make([]base.Node, 0, 3)
-	acceptmsg, ok := message.(*AcceptRequest)
+	//newNodes := make([]base.Node, 0, 3)
+	prepmsg, ok := message.(*ProposeRequest)
 	var flag bool
 	if ok {
-		// Accept Phase
+		// Propose Phase
+		newNode := server.copy()
+		if prepmsg.N > newNode.n_p {
+			newNode.n_p = prepmsg.N
+			flag = true
+
+		} else {
+			flag = false
+		}
+		res := &ProposeResponse{
+			CoreMessage: base.MakeCoreMessage(prepmsg.To(), prepmsg.From()),
+			Ok:          flag,
+			N_p:         newNode.n_p,
+			N_a:         newNode.n_a,
+			V_a:         newNode.v_a,
+			SessionId:   prepmsg.SessionId,
+		}
+		newNode.SetSingleResponse(res)
+		return []base.Node{newNode}
+	}
+
+	proposeresponse, ok := message.(*ProposeResponse)
+	if ok {
+		// Propose Response
+		// index of the peer which sent the response
+		var peer_idx int
+		for idx, peer := range server.peers {
+			if proposeresponse.From() == peer {
+				peer_idx = idx
+				break
+			}
+		}
+
+		newNodes := make([]base.Node, 0, 3)
+
+		// Update responses
+		newNode := server.copy()
+		newNode.proposer.ResponseCount++
+		newNode.proposer.Responses[peer_idx] = true
+
+		if proposeresponse.N_p > newNode.n_p {
+			newNode.n_p = proposeresponse.N_p
+		}
+		if proposeresponse.Ok {
+			newNode.proposer.SuccessCount++
+			if proposeresponse.N_a > newNode.proposer.N_a_max {
+				newNode.proposer.N_a_max = proposeresponse.N_a
+				newNode.proposer.V = proposeresponse.V_a
+			}
+		}
+
+		if newNode.proposer.SuccessCount >= len(server.peers)/2+1 {
+			// Make a subnode to move to Accept Phase
+			acceptNode := newNode.copy()
+			acceptNode.proposer.Responses = make([]bool, len(server.peers))
+			acceptNode.proposer.ResponseCount = 0
+			acceptNode.proposer.SuccessCount = 0
+			acceptNode.proposer.Phase = Accept
+
+			messages := make([]base.Message, 0, 3)
+			for _, peer := range newNode.peers {
+				req := &AcceptRequest{
+					CoreMessage: base.MakeCoreMessage(server.Address(), peer),
+					N:           newNode.proposer.N,
+					V:           newNode.proposer.V,
+					SessionId:   newNode.proposer.SessionId,
+				}
+				messages = append(messages, req)
+			}
+			acceptNode.SetResponse(messages)
+			newNodes = append(newNodes, acceptNode)
+		}
+
+		newNodes = append(newNodes, newNode)
+		return newNodes
+	}
+	acceptmsg, ok := message.(*AcceptRequest)
+	if ok {
+		// Accept phase
+
 		newNode := server.copy()
 		if acceptmsg.N >= newNode.n_p {
 			newNode.n_p = acceptmsg.N
@@ -84,33 +163,112 @@ func (server *Server) MessageHandler(message base.Message) []base.Node {
 		} else {
 			flag = false
 		}
+		//fmt.Println("---------------------------Here-----------------------------")
+		message := make([]base.Message, 0, 1)
 		res := &AcceptResponse{
 			CoreMessage: base.MakeCoreMessage(acceptmsg.To(), acceptmsg.From()),
 			Ok:          flag,
 			N_p:         newNode.n_p,
 			SessionId:   acceptmsg.SessionId,
 		}
-		newNode.SetResponse(res)
+		message = append(message, res)
+		newNode.SetResponse(message)
 		return []base.Node{newNode}
+
 	}
 
-	proposemsg, ok := message.(*ProposeRequest)
+	acceptresponse, ok := message.(*AcceptResponse)
 	if ok {
-		// Propose phase
+		// Accept reply
+		var peer_idx int
+		for idx, peer := range server.peers {
+			if acceptresponse.From() == peer {
+				peer_idx = idx
+				break
+			}
+		}
 
+		newNodes := make([]base.Node, 0, 3)
+
+		// Update responses
+		newNode := server.copy()
+		newNode.proposer.ResponseCount++
+		newNode.proposer.Responses[peer_idx] = true
+
+		if acceptresponse.N_p > newNode.n_p {
+			newNode.n_p = acceptresponse.N_p
+		}
+		if acceptresponse.Ok {
+			newNode.proposer.SuccessCount++
+		}
+
+		if newNode.proposer.SuccessCount >= len(server.peers)/2+1 {
+			// Make a subnode to move to Decide Phase
+			decideNode := newNode.copy()
+			decideNode.proposer.Responses = make([]bool, len(server.peers))
+			decideNode.proposer.ResponseCount = 0
+			decideNode.proposer.SuccessCount = 0
+			decideNode.proposer.Phase = Decide
+
+			messages := make([]base.Message, 0, 3)
+			for _, peer := range newNode.peers {
+				req := &DecideRequest{
+					CoreMessage: base.MakeCoreMessage(server.Address(), peer),
+					V:           newNode.proposer.V,
+					SessionId:   newNode.proposer.SessionId,
+				}
+				messages = append(messages, req)
+			}
+			decideNode.SetResponse(messages)
+			newNodes = append(newNodes, decideNode)
+		}
+
+		newNodes = append(newNodes, newNode)
+		return newNodes
 	}
 
 	decidemsg, ok := message.(*DecideRequest)
 	if ok {
-		// Propose phase
+		// Decide phase
+		newNode := server.copy()
+		newNode.agreedValue = decidemsg.V
+		//newNodes := make([]base.Node, 0, 1)
+		//messages := make([]base.Message, 0, 3)
+		//for _, peer := range newNode.peers {
+		//	req := &DecideResponse{
+		//		CoreMessage: base.MakeCoreMessage(server.Address(), peer),
+		//		Ok:          true,
+		//		SessionId:   newNode.proposer.SessionId,
+		//	}
+		//	messages = append(messages, req)
+		//}
+		//newNode.SetResponse(messages)
+		//newNodes = append(newNodes, newNode)
+		return []base.Node{newNode}
 	}
-
+	return nil
 }
 
 // To start a new round of Paxos.
 func (server *Server) StartPropose() {
 	//TODO: implement it
-	panic("implement me")
+	messages := make([]base.Message, 0, 3)
+	//server.n_p = 0
+	server.proposer.V = server.proposer.InitialValue
+	server.proposer.Phase = Propose
+	server.proposer.N = server.n_p + 1
+	server.proposer.SessionId++
+	server.proposer.SuccessCount = 0
+	server.proposer.ResponseCount = 0
+	for _, peer := range server.peers {
+		proposemsg := &ProposeRequest{
+			CoreMessage: base.MakeCoreMessage(server.peers[server.me], peer),
+			N:           server.proposer.N,
+			SessionId:   server.proposer.SessionId,
+		}
+		messages = append(messages, proposemsg)
+	}
+	server.SetResponse(messages)
 }
 
 // Returns a deep copy of server node
